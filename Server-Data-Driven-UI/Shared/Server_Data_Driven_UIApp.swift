6a546729a3fe51a41a2e70a6ebc94f4e694e185f
Vidhyadharan Mohanram <vid@tawk.to>
2022-05-29 11:42:00 +0530

    Initial Commit

---
+++ Server-Data-Driven-UI/Shared/Server_Data_Driven_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Server_Data_Driven_UIApp.swift
+//  Shared
+//
+//  Created by Vidhyadharan Mohanram on 29/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Server_Data_Driven_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
